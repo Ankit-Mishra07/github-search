65fec5b6c3801f532dda2e807792bb57aa030cbe
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-05-06 07:08:14 +0530

    creating footer tag

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
+import Footer from "./components/Footer";
 import Routing from "./Routes/Routing";
 
 function App() {
   return (
     <>
       <Routing />
+      <Footer/>
     </>
   );
 }
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <Box sx={{position:"fixed", bottom:0, textAlign:"center", width:"100%"}}>
+        Made with ❤ by Ankit Mishra
+    </Box>
+    </>
+  )
+}
+
+export default Footer
